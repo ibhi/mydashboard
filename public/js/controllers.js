7a82e37a4a29e20fc139b9cbf24425dc14a1cf0f
ibhi <ibrahimin2004@gmail.com>
2015-04-12 15:10:30 +0530

    Calender events connected to the full calendar todo: Connect multiple calender instance and push the events to fullcal

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -252,9 +252,38 @@ angular.module('dash.controllers',[])
 
     }
 
-    function fullCal(events){
-
+    function fullCal(entries){
+        // var eventsList = [];
+        entries.forEach(function(entry){
+            $scope.events.push({
+                id: entry.id,
+                title: entry.summary,
+                start: entry.start.dateTime || entry.start.date, 
+                end: entry.end.dateTime || entry.end.date,
+                url: entry.htmlLink,
+                location: entry.location,
+                description: entry.description
+            })
+        })
+        console.log($scope.events);
+        $scope.eventSources = [$scope.events];
     }
+
+    var date = new Date();
+    var d = date.getDate();
+    var m = date.getMonth();
+    var y = date.getFullYear();
+
+    $scope.events = [
+      // {title: 'All Day Event',start: new Date(y, m, 1)},
+      {title: 'Long Event',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
+      {id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false}
+      // {id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false},
+      // {title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false},
+      // {title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
+    ];
+
+    $scope.eventSources = [$scope.events];
 
 }])
 
